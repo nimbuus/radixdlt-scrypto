976906232acea816632b5a7f40acb30edfe41e77
nimbuus <szamoscs@hotmail.com>
2022-07-23 15:35:05 +0200

    demo1

--- token-sale/src/lib.rs
+++ token-sale/src/lib.rs
@@ -1,36 +1,52 @@
 use scrypto::prelude::*;
 
 blueprint! {
-    struct Hello {
-        // Define what resources and data will be managed by Hello components
-        sample_vault: Vault
+    struct TokenSale {
+        useful_tokens_vault: Vault,
+        xrd_tokens_vault: Vault,
+        price_per_token: Decimal
     }
 
-    impl Hello {
-        // Implement the functions and methods which will manage those resources and data
-        
-        // This is a function, and can be called directly on the blueprint once deployed
-        pub fn instantiate_hello() -> ComponentAddress {
-            // Create a new token called "HelloToken," with a fixed supply of 1000, and put that supply into a bucket
-            let my_bucket: Bucket = ResourceBuilder::new_fungible()
-                .metadata("name", "HelloToken")
-                .metadata("symbol", "HT")
-                .initial_supply(1000);
-
-            // Instantiate a Hello component, populating its vault with our supply of 1000 HelloToken
+    impl TokenSale {
+        pub fn new(price_per_token: Decimal) -> ComponentAddress {
+            let bucket: Bucket = ResourceBuilder::new_fungible()
+            .metadata("name", "Budenz")
+            .metadata("symbol", "BDZ")
+            .metadata("team-member-1-ticket-number", "#4126755519")
+            .metadata("team-member-2-ticket-number", "#2")
+            .metadata("team-member-3-ticket-number", "#3")
+            .metadata("team-member-4-ticket-number", "#4")
+            .initial_supply(100_000);
+            /* 
+            let seller_badge:Bucket ResourceBuilder :: new_fungible()
+            .divisibility(DIVISIBILITY_NONE)
+            .metadata("name","Seller Badge")
+            .metadata("symbol","SELLER")
+             initial_supply(1);
+            */
             Self {
-                sample_vault: Vault::with_bucket(my_bucket)
+                useful_tokens_vault: Vault::with_bucket(bucket),
+                xrd_tokens_vault: Vault::new(RADIX_TOKEN),
+                price_per_token: price_per_token
             }
             .instantiate()
             .globalize()
         }
 
-        // This is a method, because it needs a reference to self.  Methods can only be called on components
-        pub fn free_token(&mut self) -> Bucket {
-            info!("My balance is: {} HelloToken. Now giving away a token!", self.sample_vault.amount());
-            // If the semi-colon is omitted on the last line, the last value seen is automatically returned
-            // In this case, a bucket containing 1 HelloToken is returned
-            self.sample_vault.take(1)
+        pub fn buy(&mut self, funds: Bucket) -> Bucket {
+            let purchase_amount: Decimal = funds.amount() / self.price_per_token;
+            self.xrd_tokens_vault.put(funds);
+            self.useful_tokens_vault.take(purchase_amount)
         }
+        /* 
+        pub fn withdraw_funds(&mut self, amount: Decimal) -> Bucket {
+            // Your `withdraw_funds` implementation goes here.
+        }
+  
+        pub fn change_price(&mut self, price: Decimal) {
+            // Your `change_price` implementation goes here.
+        }
+        */
     }
 }
+
